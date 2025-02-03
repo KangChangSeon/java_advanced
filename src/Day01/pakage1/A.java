ac1b83fa4a06b06808c7ca1cf3f7f8ae40f7cef3
KangChangSeon <rlckdtjs@naver.com>
2025-02-03 14:07:00 +0900

    commit

---
+++ src/Day01/pakage1/A.java
@@ -0,0 +1,11 @@
+package Day01.pakage1;
+
+public class A {
+    protected String field1;
+
+    protected A(){}
+
+    protected  void method1(){
+        System.out.println("A 클래스의 method1()");
+    }
+}
