40a73639647e67d6d258cd320f0eab5b8b3848ec
KangChangSeon <rlckdtjs@naver.com>
2025-02-04 10:54:57 +0900

    commit

--- src/Day01/Quiz03/Wheeler.java
+++ src/Day01/Quiz03/Wheeler.java
@@ -49,4 +49,15 @@ public abstract class Wheeler {
         System.out.println("정지상태로 속도를 0으로 초기화합니다.");
     }
 }
-
+//public abstract class Wheeler {
+//    protected int velocity;
+//    protected String carName;
+//    protected int wheelNumber;
+//
+//    public abstract void speedUp(int speed);
+//    public abstract void speedDown(int speed);
+//    public void stop(){
+//        this.velocity = 0;
+//        System.out.println("정지상태로 속도를 " + this.velocity + "으로 초기화합니다.");
+//    }
+//}
