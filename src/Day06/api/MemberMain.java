dc54d0fe9ade9991dce8eda3bd84d6b3f06698ef
KangChangSeon <rlckdtjs@naver.com>
2025-02-10 12:03:21 +0900

    commit

--- src/Day06/api/MemberMain.java
+++ src/Day06/api/MemberMain.java
@@ -16,8 +16,14 @@ public class MemberMain {
         System.out.println(m1.equals(m2)); // Object equals() : 주소값을 비교하기 때문에 false
 
         // 객체 인스턴스마다 각기 다른 해시코드(주소) 값을 가지기 때문이다.
+        // 현재, Member 클래스에서 equals() 와 hashcode()를 오버라이딩 했기 때문에,
+        // 두 객체 필드 name의 해시코드가 반환 값이다.
         System.out.println(m1.hashCode());
         System.out.println(m2.hashCode());
+
+        //순수 객체의 주소를 얻고 싶을 때는 identifyHashCode()
+        System.out.println(System.identityHashCode(m1));
+        System.out.println(System.identityHashCode(m2));
 
         // 리스트를 생성하고 두 객체 데이터 추가
         List<Member> members = new ArrayList<>(); // 비교하지 않고 바로 집어넣음
@@ -31,6 +37,6 @@ public class MemberMain {
         members1.add(m2);
         System.out.println(members1.size());
         // Collection(HashMap, HashSet, HasTable) 은 객체가 논리적으로 같은지를 비교할 때
-        // hashcode() -검증 후-> equals()
+        // hashcode() -검증 후-> equals()equals
     }
 }
---
+++ src/Day06/api/UserMain.java
@@ -0,0 +1,80 @@
+package Day06.api;
+
+import java.util.Arrays;
+
+public class UserMain {
+    public static void main(String[] args) {
+
+//        // 얕은 복사(shallow copy : 주소값 복사)
+//        User user = new User();
+//        user.setName("Shin");
+//        User copy = user;
+//
+//        System.out.println(user.hashCode());
+//        System.out.println(copy.hashCode());
+//        System.out.println(user.equals(copy));
+//
+//        // 깊은 복사(deep copy : 다른 heap 데이터를 가짐)
+//        try {
+//            User user1 = new User();
+//            user1.setName("Shin");
+//            User copy1 = (User)user1.clone();
+//
+//            System.out.println(user1.hashCode());
+//            System.out.println(copy1.hashCode());
+//            System.out.println(user1.equals(copy));
+//        } catch (CloneNotSupportedException e) {
+//            throw new RuntimeException(e);
+//        }
+        User[] arrayObj = {new User(100,"park"),new User(101,"kim"),new User(102,"kang")};
+        System.out.println(Arrays.toString(arrayObj));
+
+//        User[] arrayObj1; // 복제할 array 선언
+//        arrayObj1 = arrayObj.clone();
+        User[] arrayObj1 = new User[3]; // 복제할 array 선언
+        for (int i = 0; i < arrayObj1.length; i++) {
+            try {
+                arrayObj1[i] = (User)arrayObj[i].clone();
+            } catch (CloneNotSupportedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        System.out.println(Arrays.toString(arrayObj1));
+
+        System.out.println(arrayObj[0].getId());
+        System.out.println(arrayObj1[0].getId());
+        arrayObj1[0].setId(999); // 복사한 arrayObj1의 첫번째 객체의 멤버값을 변경
+
+        System.out.println(arrayObj[0].getId());
+        System.out.println(arrayObj1[0].getId());
+
+        // 복제되는 대상이 배열의 요소 데이터인 Object.clone()을 오버라이딩 해줌으로써
+        // 직접 for문을 돌며 객체 복사를 하는 것과 같다.
+    }
+}
+/*
+1078694789
+1078694789
+true
+1831932724
+1747585824
+false
+ */
+
+/*
+[Day06.api.User@404b9385, Day06.api.User@6d311334, Day06.api.User@682a0b20]
+[Day06.api.User@404b9385, Day06.api.User@6d311334, Day06.api.User@682a0b20]
+100
+100
+999
+999
+ */
+
+/*
+[Day06.api.User@404b9385, Day06.api.User@6d311334, Day06.api.User@682a0b20]
+[Day06.api.User@3d075dc0, Day06.api.User@214c265e, Day06.api.User@448139f0]
+100
+100
+100
+999
+ */
