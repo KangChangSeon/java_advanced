c9347b13d82266b92efbf1058a5bee9b2966734e
KangChangSeon <rlckdtjs@naver.com>
2025-02-14 16:40:57 +0900

    commit

---
+++ src/workshop/BoardExample.java
@@ -0,0 +1,63 @@
+package workshop;
+
+import lombok.Data;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+@Data
+public class BoardExample {
+    private int no;
+    private String writer;
+    private Date date;
+    private String title;
+    private String formattedDate;
+
+    static List<BoardExample> boardList = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
+
+    public BoardExample(int no, String writer, String title) {
+        this.no = no;
+        this.writer = writer;
+        this.title = title;
+        this.date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        this.formattedDate = sdf.format(date); // 변환된 날짜 문자열 저장
+    }
+
+    void mainMenu() {
+        int choice = 0;
+        System.out.println("메인 메뉴: 1.Create | 2.Read | 3.Clear | 4.Exit");
+        System.out.print("메뉴 선택: ");
+        choice = scanner.nextInt();
+        System.out.println();
+        switch (choice) {
+            case 1:
+                System.out.println("***");
+        }
+    }
+
+    void list() {
+        System.out.println("[게시물 목록]");
+        System.out.println("-----------------------------------------------------------");
+        System.out.printf("%-7s %-10s %-12s %-30s\n", "no", "writer", "date", "title");
+        System.out.println("-----------------------------------------------------------");
+        for (BoardExample board : boardList) {
+            System.out.printf("%-7d %-10s %-12s %-30s\n",
+                    board.no, board.writer, board.formattedDate, board.title);
+        }
+        System.out.println("-----------------------------------------------------------");
+        mainMenu();
+    }
+
+    public static void main(String[] args) {
+        BoardExample writer1 = new BoardExample(1, "winter", "게시판에 오신 것을 환영합니다.");
+        BoardExample writer2 = new BoardExample(1, "winter", "올 겨울은 많이 춥습니다.");
+        boardList.add(writer1);
+        boardList.add(writer2);
+        writer1.list();
+    }
+}
