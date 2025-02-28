3fdfbc34fbd08f9902bdf0f1431d9ef659713054
KangChangSeon <rlckdtjs@naver.com>
2025-02-28 14:34:20 +0900

    commit

--- src/jdbc/advanced2/users/DBUtil.java
+++ src/jdbc/advanced2/users/DBUtil.java
@@ -34,3 +34,42 @@ public class DBUtil {
 
     private DBUtil() {}
 }
+//
+//package jdbc.advanced.users;
+//
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+//
+//import static java.lang.Class.forName;
+//
+///**
+// * packageName   : jdbc.advanced.users
+// * fileName      : DBUtil
+// * author        : a
+// * date          : 2025-02-28
+// * description   : 데이터베이스 연결 작업 클래스
+// * =================================================
+// * DATE             AUTHOR             NOTE
+// * -------------------------------------------------
+// * 2025-02-28        a
+// */
+//public class DBUtil {
+//    private static Connection connection = null;
+//
+//    /**
+//     * Gets connection.
+//     *
+//     * @return the connection
+//     */
+//    public static Connection getConnection() {
+//
+//        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ssgdb?serverTimezone=Asia/Seoul", "ssg", "ssg1234");
+//        } catch (ClassNotFoundException | SQLException e) {
+//            System.err.println(e.getMessage());
+//        }
+//        return connection;
+//    }
+//}
